1de37cf360ab384553980eeb8ee7f4ceb118596c
Christian Plesner Hansen <c7n@p5r.org>
2026-07-05 22:42:46 +0200

    wip

--- weeks.py
+++ weeks.py
@@ -1,11 +1,11 @@
 from openscad import *
 
-fn = 50
+fn = 100
 
 outer_radius = 24
 wall_width = 1
 outer_ring_inner_radius = outer_radius - 8
-outer_ring_height = 4.8
+outer_ring_height = 4.6
 outer_fixpoint_count = 13
 
 eps = 0.01
@@ -32,22 +32,36 @@ fixpoint = linear_extrude(
       angle=180 / outer_fixpoint_count), 
     circle(
       r=outer_radius - 2 * wall_width)),
-  height=3.2)
+  height=3)
 
-locked_outer_groove = linear_extrude(
+fixpoints = union([
+  fixpoint.rotate([0, 0, (i + 0.25) * 360 / outer_fixpoint_count])
+    for i in range(0, outer_fixpoint_count)])
+
+dimple = cylinder(d1=2, d2=0, h=1) + [outer_radius - 4, 0, -eps]
+
+dimples = union([
+  dimple.rotate([0, 0, i * 360 / outer_fixpoint_count])
+    for i in range(0, outer_fixpoint_count)])
+
+engaged_outer_groove = linear_extrude(
   difference(
-    circle(
-      r=outer_radius - 3,
-      angle=360 / outer_fixpoint_count), 
+    rotate(
+      circle(
+        r=outer_radius - 3,
+        angle=360 / outer_fixpoint_count + 2 * eps),
+        [-eps]),
     circle(
       r=outer_radius - 5)),
   height=3) + [0, 0, 1]
 
-engaged_outer_groove = linear_extrude(
+locked_outer_groove = linear_extrude(
   difference(
-    circle(
-      r=outer_radius - 5,
-      angle=360 / outer_fixpoint_count), 
+    rotate(
+      circle(
+        r=outer_radius - 5,
+        angle=360 / outer_fixpoint_count + 2 * eps),
+      [-eps]), 
     circle(
       r=outer_radius - 7)),
   height=3) + [0, 0, 1]
@@ -68,16 +82,12 @@ groove_pattern = [
   locked_outer_groove
 ]
 
-fixpoints = union([
-  fixpoint.rotate([0, 0, i * 360 / outer_fixpoint_count])
-    for i in range(0, outer_fixpoint_count)])
-
 grooves = union([
   groove_pattern[i].rotate([0, 0, i * 360 / outer_fixpoint_count])
     for i in range(0, outer_fixpoint_count)])
 
 root = union(
-  difference(outer_ring_bottom, grooves),
+  difference(outer_ring_bottom, grooves, dimples),
   outer_ring_outer_wall,
   fixpoints)
 
